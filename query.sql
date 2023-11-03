90a1fee22a0d019ad49294c96b8abc29c4049d77
ducngo123456 <135568147+duc-ngo-saltovn@users.noreply.github.com>
2023-11-03 16:01:11 +0700

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,9 @@
+-- Employees 
+CREATE TABLE Employees (
+    ID INT AUTO_INCREMENT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Age INT,
+    PRIMARY KEY (ID)
+);
